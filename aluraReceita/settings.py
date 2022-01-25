04e225408021ae2685c78780bf912cdcdfd71214
vgarciasouza <vgarciasouza@hotmail.com>
2022-01-25 10:50:31 -0300

    Quem postou a receita

--- aluraReceita/settings.py
+++ aluraReceita/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pessoas',
     'receitas',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ pessoas/admin.py
@@ -0,0 +1,15 @@
+from re import search
+from django.contrib import admin
+from .models import Pessoa
+
+
+class ListandoPessoa(admin.ModelAdmin):
+    list_display = ('id','nome', 'email')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'email')
+    list_per_page = 20
+
+
+admin.site.register(Pessoa, ListandoPessoa)
+
+
